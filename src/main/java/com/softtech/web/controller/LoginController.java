b461ff1a3657e46436ad60ac65ff7e44a6d4719c
josephbagnes <jbrbagnes@ymail.com>
2014-07-07 02:53:59 +0100

    thymeleaf and slf4j

--- src/main/java/com/softtech/web/controller/LoginController.java
+++ src/main/java/com/softtech/web/controller/LoginController.java
@@ -1,12 +1,18 @@
 package com.softtech.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
 	@RequestMapping("/login")
-	public String doLogin(){
+	public String login(){
+		return "login";
+	}
+	@RequestMapping("/login-error")
+	public String loginError(Model model){
+		model.addAttribute("loginError", true);
 		return "login";
 	}
 }
