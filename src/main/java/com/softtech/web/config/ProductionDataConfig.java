c2ed3797ede197633d92709c3c4c509e29317234
gengstah <gerardpdelasarmas@gmail.com>
2014-07-20 14:33:58 +0800

    Added dialect to ProductionDataConfig

--- src/main/java/com/softtech/web/config/ProductionDataConfig.java
+++ src/main/java/com/softtech/web/config/ProductionDataConfig.java
@@ -1,14 +1,16 @@
 package com.softtech.web.config;
 
+import java.util.Properties;
+
 import javax.inject.Inject;
 import javax.sql.DataSource;
 
-import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.softtech.web.annotation.Production;
@@ -28,19 +30,24 @@ public class ProductionDataConfig extends AbstractDataConfig {
 	@Bean
 	public DataSource dataSource() {
 		
-		/*DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setUrl(env.getProperty("jdbc.url"));
 		dataSource.setUsername(env.getProperty("jdbc.username"));
 		dataSource.setPassword(env.getProperty("jdbc.password"));
-		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));*/
-		
-		BasicDataSource dataSource = new BasicDataSource();
 		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
-		dataSource.setUrl(env.getProperty("jdbc.url"));
-		dataSource.setUsername(env.getProperty("jdbc.username"));
-		dataSource.setPassword(env.getProperty("jdbc.password"));
-		
+
 		return dataSource;
+		
+	}
+	
+	@Override
+	protected Properties additionalProperties() {
+		
+		Properties properties = new Properties();	
+		properties.setProperty("hibernate.hbm2ddl.auto", "none");
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+		
+		return properties;
 		
 	}
 	
