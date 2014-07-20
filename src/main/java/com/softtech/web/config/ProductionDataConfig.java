590396a92dacceed5be2c98debd40f2d69eb9ab1
gengstah <gerardpdelasarmas@gmail.com>
2014-07-20 14:27:40 +0800

    changed datasource implementation to apache BasicDatasource

--- src/main/java/com/softtech/web/config/ProductionDataConfig.java
+++ src/main/java/com/softtech/web/config/ProductionDataConfig.java
@@ -3,12 +3,12 @@ package com.softtech.web.config;
 import javax.inject.Inject;
 import javax.sql.DataSource;
 
+import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.softtech.web.annotation.Production;
@@ -28,12 +28,18 @@ public class ProductionDataConfig extends AbstractDataConfig {
 	@Bean
 	public DataSource dataSource() {
 		
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		/*DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setUrl(env.getProperty("jdbc.url"));
 		dataSource.setUsername(env.getProperty("jdbc.username"));
 		dataSource.setPassword(env.getProperty("jdbc.password"));
+		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));*/
+		
+		BasicDataSource dataSource = new BasicDataSource();
 		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
-
+		dataSource.setUrl(env.getProperty("jdbc.url"));
+		dataSource.setUsername(env.getProperty("jdbc.username"));
+		dataSource.setPassword(env.getProperty("jdbc.password"));
+		
 		return dataSource;
 		
 	}
