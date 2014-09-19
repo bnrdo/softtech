0d1df6bae8c485dcb3f51a8bd60f4a003ab42785
josephbagnes <josephbagnes@gmail.com>
2014-09-19 21:46:08 +0800

    reset page changes

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -2,4 +2,20 @@ $(document).ready(function () {
 	$('#logoutLink').click(function() {
 		$('#formLogout').submit();
 	});
+	$('#submitReset').click(function(){
+		if($('#password').val() !== $('#confirmPassword').val()){
+			$('#resetErrorDiv').fadeIn(1000).delay(2000).fadeOut("slow");
+			return false;
+		}
+	});	
+	$('.btn-file :file').on('fileselect', function(event, numFiles, label) {    
+		var input = $(this).parents('.input-group').find(':text'),
+            log = numFiles > 1 ? numFiles + ' files selected' : label;
+        
+        if( input.length ) {
+            input.val(log);
+        } else {
+            if( log ) alert(log);
+        }        
+    });
 });
